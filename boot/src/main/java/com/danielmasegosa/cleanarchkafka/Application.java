992d2870920236be60a7925257fa84657e67be57
Daniel Masegosa <danielmasegosa@hotmail.com>
2019-09-29 01:07:19 +0200

    Improved clean architecture structure

--- boot/src/main/java/com/danielmasegosa/cleanarchkafka/Application.java
+++ boot/src/main/java/com/danielmasegosa/cleanarchkafka/Application.java
@@ -1,5 +1,6 @@
 package com.danielmasegosa.cleanarchkafka;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
@@ -7,6 +8,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class Application {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class);
+        SpringApplication.run(Application.class, args);
     }
 }
